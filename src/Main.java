a96e8a5d660d971696bf3a1652bd6fef50ccafea
Alexander Baklushin <din.myrk@gmail.com>
2022-09-19 17:25:42 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,74 @@
 import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "hdryjdy";
-        String password = "ацуац";
-        String confirmPassword = "ацуац";
-        receivingUserData(login,password,confirmPassword);
+        String login = "P1wrg_rR";
+        String password = "P1ewttttttttttttttttttttttttttrwe_1";
+        String confirmPassword = "P1ewtttttttttttttttttttttttttrwe_1";
+        System.out.println(userDataVerification(login,password,confirmPassword));
     }
 
-    public static boolean receivingUserData(String login,String password,String confirmPassword){
-        boolean loginSymbol = login.matches("^[a-z0-9_]+$");
-        boolean passwordSymbol = password.matches("^[a-zA-Z0-9_]+$");
-        boolean passwordComparison = confirmPassword.equals(password);
-        if ((!loginSymbol) || (login.length() > 20)) {
-            try {
-                throw new WrongLoginException("Ошибка ввода");
-            } catch (WrongLoginException e) {
-                System.out.println("Error");
-            } finally {
-                return false;
-            }
-        } else if ((!passwordSymbol) || (password.length() >= 20)) {
-            try {
+    public static boolean userDataVerification(String login,String password,String confirmPassword){
+        boolean userLoginVerification;
+        boolean loginLengthCheck;
+        boolean userPasswordVerification;
+        boolean passwordLengthCheck;
+        boolean comparePasswordAndConfirmPassword;
+        try {
+            userLoginVerification = userLoginVerification(login);
+            loginLengthCheck = loginLengthCheck(login);
+            userPasswordVerification = userPasswordVerification(password);
+            passwordLengthCheck = passwordLengthCheck(password);
+            comparePasswordAndConfirmPassword = comparePasswordAndConfirmPassword(password,confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return userLoginVerification
+                && loginLengthCheck
+                && userPasswordVerification
+                && passwordLengthCheck
+                && comparePasswordAndConfirmPassword;
+    }
 
-                throw new WrongLoginException("Ошибка ввода");
-            } catch (WrongLoginException e) {
-                System.out.println("Error");
-            } finally {
-                return false;
-            }
-        } else if (!passwordComparison) {
-            try {
-                throw new WrongLoginException("Ошибка ввода");
-            } catch (WrongLoginException e) {
-                System.out.println("Error");
-            } finally {
-                return false;
-            }
-        } else System.out.println("Пользователь зарегистрирован");
+    public static boolean userLoginVerification(String login) throws NullPointerException{
+        if (login == null){
+            throw new NullPointerException();
+        }
+        if (login.matches("\\w+")){
+            return true;
+        }
+        System.out.println("invalid characters in login");
+        return false;
+    }
+
+    public static boolean loginLengthCheck(String login) throws WrongLoginException{
+        if(login.length() > 20){
+            throw new WrongLoginException("login length is more than 20 characters");
+        }
+        return true;
+    }
+
+    public static boolean userPasswordVerification(String password) throws NullPointerException{
+        if (password == null){
+            throw new NullPointerException();
+        }
+        if (password.matches("\\w+")){
+            return true;
+        }
+        System.out.println("invalid characters in password");
+        return false;
+    }
+
+    public static boolean passwordLengthCheck(String password){
+        return password.length() <= 19;
+    }
+
+    public static boolean comparePasswordAndConfirmPassword(String password,String confirmPassword) throws WrongPasswordException{
+        if (!password.equals(confirmPassword)){
+            throw new WrongPasswordException("passwords are not the same");
+        }
         return true;
     }
 }
